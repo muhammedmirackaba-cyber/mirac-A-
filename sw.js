e6d373e82299e25a4ef2b5a9d6c61d54864fb4eb
muhammedmirackaba-cyber <muhammedmirackaba@gmail.com>
2026-03-22 09:31:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'mirkay-ai-v4';
+const urlsToCache = [
+  '/mirac-A-/',
+  '/mirac-A-/index.html',
+  '/mirac-A-/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
